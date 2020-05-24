82812072a05ab7178df5452916fad31d779416ba
RedHot099 <jkbeichner@gmail.com>
2020-05-25 01:41:53 +0200

    ASP.NET MVC
    ASP.NET MVC

---
+++ lab9/MVC/MVC/Models/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+
+namespace MVC.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the name of a song is 100 characters!")]
+        public string Name { set; get; }
+        public Song ICollection { set; get; }
+    }
+}
---
+++ lab9/MVC/MVC/Models/MusicDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class MusicDbContext : System.Data.Entity.DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
